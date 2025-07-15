122d9878950f53e841c2ed1b8be51ddb30f292eb
Fabio Kusaba <fabinhokb44@gmail.com>
2025-07-15 20:55:03 -0300

    first commit

---
+++ src/test/java/com/fabiokusaba/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiokusaba.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
