9683488eb0f6a33f2813e97dbab129a106bcc889
Fabio Kusaba <fabinhokb44@gmail.com>
2025-07-19 21:32:27 -0300

    (feat): adicionar repositorio UserRepository

---
+++ src/main/java/com/fabiokusaba/curso/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.fabiokusaba.curso.repositories;
+
+import com.fabiokusaba.curso.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
